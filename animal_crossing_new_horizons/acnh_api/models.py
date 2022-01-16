5d4abfce352f1d3af9f3e3c4365e0b366a34207d
dnlgrwd87 <dnlgrwd@gmail.com>
2022-01-16 10:50:10 -0500

    Add models for fish and insects

--- animal_crossing_new_horizons/acnh_api/models.py
+++ animal_crossing_new_horizons/acnh_api/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
-class Villager(models.Model):
+class BaseModel(models.Model):
+    spreadsheet_id = models.CharField(max_length=256, unique=True)
+    color1 = models.CharField(max_length=256)
+    color2 = models.CharField(max_length=256)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def colors(self):
+        return [self.color1, self.color2]
+
+
+class Villager(BaseModel):
     name = models.CharField(max_length=256)
     icon_image = models.CharField(max_length=256)
     house_image = models.CharField(max_length=256)
@@ -12,34 +25,48 @@ class Villager(models.Model):
     catchphrase = models.CharField(max_length=256)
     style1 = models.CharField(max_length=256)
     style2 = models.CharField(max_length=256)
-    color1 = models.CharField(max_length=256)
-    color2 = models.CharField(max_length=256)
-    spreadsheet_id = models.CharField(max_length=256)
 
     @property
     def styles(self):
         return [self.style1, self.style2]
 
-    @property
-    def colors(self):
-        return [self.color1, self.color2]
 
-
-class Fossil(models.Model):
+class Fossil(BaseModel):
     name = models.CharField(max_length=256)
     image = models.CharField(max_length=256)
+    museum_description = models.CharField(max_length=1000)
     buy_price = models.IntegerField()
     sell_price = models.IntegerField()
     size = models.CharField(max_length=256)
     source = models.CharField(max_length=256)
-    color1 = models.CharField(max_length=256)
-    color2 = models.CharField(max_length=256)
     interactive = models.BooleanField()
     can_reorder = models.BooleanField()
     in_catalog = models.BooleanField()
-    museum_description = models.CharField(max_length=1000)
-    spreadsheet_id = models.CharField(max_length=256)
 
     @property
     def colors(self):
         return [self.color1, self.color2]
+
+
+class BaseCritter(BaseModel):
+    name = models.CharField(max_length=256)
+    critterpedia_image = models.CharField(max_length=256)
+    icon_image = models.CharField(max_length=256)
+    furniture_image = models.CharField(max_length=256)
+    museum_description = models.CharField(max_length=1000)
+    catch_phrase = models.CharField(max_length=1000)
+    source = models.CharField(max_length=1000)
+    sell_price = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Fish(BaseCritter):
+    lighting_type = models.CharField(max_length=256)
+    shadow = models.CharField(max_length=256)
+    size = models.CharField(max_length=256)
+
+
+class Insect(BaseCritter):
+    weather = models.CharField(max_length=256)
