fdfe20d97b753ccafbe798f8662d505b9d316e84
dnlgrwd87 <dnlgrwd@gmail.com>
2022-01-16 10:08:12 -0500

    change pagination limit and dynamically set DEBUG setting

--- animal_crossing_new_horizons/settings/django.py
+++ animal_crossing_new_horizons/settings/django.py
@@ -7,11 +7,9 @@ environ.Env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
--- animal_crossing_new_horizons/settings/rest_framework.py
+++ animal_crossing_new_horizons/settings/rest_framework.py
@@ -10,5 +10,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25,
-    'max_page_size': 1000,
+    'max_page_size': 200,
 }
