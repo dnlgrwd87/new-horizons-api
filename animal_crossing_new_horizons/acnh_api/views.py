78756dccba692494f980093434b38c81e3e033d2
dnlgrwd87 <dnlgrwd@gmail.com>
2022-07-05 00:33:41 -0400

    add environ library and remove id ordering on viewsets

--- animal_crossing_new_horizons/acnh_api/views.py
+++ animal_crossing_new_horizons/acnh_api/views.py
@@ -1,33 +1,37 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from animal_crossing_new_horizons.acnh_api.models import Villager, Fossil, Fish, Insect
-from animal_crossing_new_horizons.acnh_api.serializers import VillagerSerializer, FossilSerializer, FishSerializer, \
+from animal_crossing_new_horizons.acnh_api.serializers import (
+    VillagerSerializer,
+    FossilSerializer,
+    FishSerializer,
     InsectSerializer
+)
 
 
 class VillagerModelViewSet(ReadOnlyModelViewSet):
-    queryset = Villager.objects.order_by('id').all()
+    queryset = Villager.objects.all()
     serializer_class = VillagerSerializer
 
     filter_fields = ('gender', 'personality')
 
 
 class FossilModelViewSet(ReadOnlyModelViewSet):
-    queryset = Fossil.objects.order_by('id').all()
+    queryset = Fossil.objects.all()
     serializer_class = FossilSerializer
 
     filter_fields = ('in_catalog',)
 
 
 class FishModelViewSet(ReadOnlyModelViewSet):
-    queryset = Fish.objects.order_by('id').all()
+    queryset = Fish.objects.all()
     serializer_class = FishSerializer
 
     filter_fields = ('sell_price',)
 
 
 class InsectModelViewSet(ReadOnlyModelViewSet):
-    queryset = Insect.objects.order_by('id').all()
+    queryset = Insect.objects.all()
     serializer_class = InsectSerializer
 
     filter_fields = ('sell_price',)
